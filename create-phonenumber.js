0438ed5828438107cbd6ae17884dbeed8bef655b
Woon Koh <woonsta13@gmail.com>
2023-04-25 14:24:37 -0700

    solution added

---
+++ create-phonenumber.js
@@ -0,0 +1,6 @@
+//write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+function createPhoneNumber(numbers){
+    let str = numbers.join('');
+    return `(${str.slice(0,3)}) ${str.slice(3,6)}-${str.slice(6,10)}`
+}
